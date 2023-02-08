6e8517c3a1fdb11026683fa90e3a6bafced1c8d0
AntonSVTSV <freshbreathe@gmail.com>
2023-02-08 15:22:10 +0700

    Add ButtonWithAction

---
+++ Modern frameworks GB/Common/Buttons/ButtonWithAction.swift
@@ -0,0 +1,34 @@
+import Foundation
+import UIKit
+
+class ButtonWithAction: UIButton {
+    
+    // MARK: - Private Properties
+    private var action: EmptyClosure?
+    
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addActionTarget()
+    }
+    
+    @available (*, unavailable)
+    required init?(coder: NSCoder) {
+        assertionFailure("init(coder:) has not been implemented")
+        return nil
+    }
+    
+    // MARK: - Public Methods
+    func setButtonAction(_ action: @escaping EmptyClosure) {
+        self.action = action
+    }
+    
+    // MARK: - Private Methods
+    private func addActionTarget() {
+        self.addTarget(self, action: #selector(onButtonTap), for: .touchUpInside)
+    }
+    
+    @objc private func onButtonTap() {
+        action?()
+    }
+}
