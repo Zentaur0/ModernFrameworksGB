94c5ba5f5edf75bd0e41b548f9835adfe866a820
AntonSVTSV <freshbreathe@gmail.com>
2023-02-13 19:08:40 +0700

    Add UserManager

---
+++ Modern frameworks GB/Managers/UserManager/UserManagerImpl.swift
@@ -0,0 +1,71 @@
+import Foundation
+import RealmSwift
+
+final class UserManagerImpl: UserManager {
+    
+    private let realmQueue: DispatchQueue
+    
+    init(realmQueue: DispatchQueue) {
+        self.realmQueue = realmQueue
+    }
+    
+    // MARK: - Private Properties
+    private let logger = Logger(component: "UserManager")
+    
+    // MARK: - UserManager
+    func loginUser(_ user: User, completion: @escaping (_ isSuccessful: Bool) -> Void) {
+        do {
+            let realm = try Realm()
+            let user = realm.object(ofType: User.self, forPrimaryKey: user.login)
+            completion(user != nil)
+        } catch {
+            logger.error(error)
+            completion(false)
+        }
+    }
+    
+    func registerUser(_ newUser: User, completion: @escaping (_ isSuccessful: Bool) -> Void) {
+        do {
+            let realm = try Realm()
+            
+            try realm.write {
+                guard isUserExists(newUser) else {
+                    realm.add(newUser)
+                    completion(isUserExists(newUser))
+                    return
+                }
+                
+                guard let existedUser = realm.object(ofType: User.self, forPrimaryKey: newUser.login) else {
+                    completion(false)
+                    return
+                }
+                existedUser.password = newUser.password
+                realm.add(existedUser, update: .modified)
+                
+                completion(isUserExists(newUser))
+            }
+        } catch {
+            logger.error(error)
+            completion(false)
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func isUserExists(_ checkingUser: User) -> Bool {
+        do {
+            let realm = try Realm()
+            let users = realm.objects(User.self)
+            
+            var isSuccess = false
+            
+            for user in users where user.login == checkingUser.login {
+                isSuccess = true
+            }
+            
+            return isSuccess
+        } catch {
+            logger.error(error)
+            return false
+        }
+    }
+}
